6ae452cc4300bd414f3a58504e65bc7fff9468b8
皮皮萌宝 <panda1943575780@outlook.com>
2024-09-21 15:34:53 +0800

    添加 README.md 文件和部分测试数据

---
+++ README.md
@@ -0,0 +1,3 @@
+# 文档说明
+
+Demo-序号为具体的模块名称，`src/test/java` 文件夹中，为测试代码，`src/test/resources` 文件夹中，为具体的 HTTP 请求，可在 IDEA 中直接运行。
---
+++ demo-04/src/test/java/xyz/ppmb/ApplicationTest.java
@@ -0,0 +1,35 @@
+package xyz.ppmb;
+
+import cn.dev33.satoken.stp.StpUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+    @Test
+    void shutdown() {
+        StpUtil.login(10001);
+        System.out.println(StpUtil.isLogin());
+        StpUtil.logout(10001);
+        System.out.println(StpUtil.isLogin());
+        StpUtil.login(10001);// 强制指定账号注销下线
+        System.out.println(StpUtil.isLogin());
+        // StpUtil.logout(10001, "PC");// 强制指定账号指定端注销下线
+        // System.out.println(StpUtil.isLogin());
+        StpUtil.logoutByTokenValue(StpUtil.getTokenValue());      // 强制指定 Token 注销下线
+        System.out.println(StpUtil.isLogin());
+    }
+    @Test
+    void kick() {
+        StpUtil.login(10001);
+        System.out.println(StpUtil.isLogin());
+        StpUtil.kickout(10001);                    // 将指定账号踢下线
+        System.out.println(StpUtil.isLogin());
+        StpUtil.login(10001);// 强制指定账号注销下线
+        System.out.println(StpUtil.isLogin());
+        // StpUtil.kickout(10001, "PC");              // 将指定账号指定端踢下线
+        // System.out.println(StpUtil.isLogin());
+        StpUtil.kickoutByTokenValue(StpUtil.getTokenValue());      // 将指定 Token 踢下线
+        System.out.println(StpUtil.isLogin());
+    }
+}
